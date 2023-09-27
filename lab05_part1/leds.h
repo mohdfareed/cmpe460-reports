5f090ca92eb9467a87b6027f0823ddd2397ac4d0
trahnt <twes1jr@gmail.com>
2023-09-27 08:26:44 -0400

    added color defines

--- lab05_part1/leds.h
+++ lab05_part1/leds.h
@@ -1,2 +1,10 @@
 void LED1_Init(void);
 void LED2_Init(void);
+
+#define RED 1      // P2.0 RED LED
+#define GREEN 2    // P2.1 GREEN LED
+#define BLUE 4     // P2.2 BLUE LED
+#define CYAN 6
+#define MAGENTA 5
+#define YELLOW 3
+#define WHITE 7
