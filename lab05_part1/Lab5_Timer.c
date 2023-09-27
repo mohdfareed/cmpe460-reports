59548341c096d4ff1dbdf4dc835e640b63598fa6
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-09-26 20:09:11 -0400

    Work on part 1

--- lab05_part1/Lab5_Timer.c
+++ lab05_part1/Lab5_Timer.c
@@ -1,12 +1,12 @@
 /*
-* Rochester Institute of Technology
-* Department of Computer Engineering
-* CMPE 460  Interfacing Digital Electronics
-* LJBeato
-* 1/14/2021
-*
-* Filename: main_timer_template.c
-*/
+ * Rochester Institute of Technology
+ * Department of Computer Engineering
+ * CMPE 460  Interfacing Digital Electronics
+ * LJBeato
+ * 1/14/2021
+ *
+ * Filename: main_timer_template.c
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -24,13 +24,12 @@ BOOLEAN g_sendData = FALSE;
 uint16_t line[128];
 
 int colorIndex = 0;
-BYTE colors[7] = { RED, GREEN, BLUE, CYAN, MAGENTA, YELLOW, WHITE };
+BYTE colors[7] = {RED, GREEN, BLUE, CYAN, MAGENTA, YELLOW, WHITE};
 
 BOOLEAN Timer1RunningFlag = FALSE;
 BOOLEAN Timer2RunningFlag = FALSE;
 
 unsigned long MillisecondCounter = 0;
-//
 
 //  I/O interrupt pin setup
 //
@@ -47,94 +46,105 @@ void Switch1_Interrupt_Init(void)
 	DisableInterrupts();
 	// initialize the Switch as per previous lab
 	Switch1_Init();
-	
- 
-	
-	//7-0 PxIFG RW 0h Port X interrupt flag
-	//0b = No interrupt is pending.
-	//1b = Interrupt is pending.
-	// clear flag1 (reduce possibility of extra interrupt)	
-  P1->IFG &= ~2;  // P1.1
 
-	//7-0 PxIE RW 0h Port X interrupt enable
-	//0b = Corresponding port interrupt disabled
-	//1b = Corresponding port interrupt enabled	
-	// arm interrupt on  P1.1	
-  P1->IE |= 2;  
+	// 7-0 PxIFG RW 0h Port X interrupt flag
+	// 0b = No interrupt is pending.
+	// 1b = Interrupt is pending.
+	//  clear flag1 (reduce possibility of extra interrupt)
+	P1->IFG &= ~2; // P1.1
 
-	//7-0 PxIES RW Undefined Port X interrupt edge select
-  //0b = PxIFG flag is set with a low-to-high transition.
-  //1b = PxIFG flag is set with a high-to-low transition
+	// 7-0 PxIE RW 0h Port X interrupt enable
+	// 0b = Corresponding port interrupt disabled
+	// 1b = Corresponding port interrupt enabled
+	//  arm interrupt on  P1.1
+	P1->IE |= 2;
+
+	// 7-0 PxIES RW Undefined Port X interrupt edge select
+	// 0b = PxIFG flag is set with a low-to-high transition.
+	// 1b = PxIFG flag is set with a high-to-low transition
+	//  now set the pin to cause falling edge interrupt event
+	//  P1.1 is falling edge event
+	P1->IES |= 2;
+
 	// now set the pin to cause falling edge interrupt event
-	// P1.1 is falling edge event
-  P1->IES |= 2; 
-	
-	// now set the pin to cause falling edge interrupt event
-  NVIC_IPR8 = (NVIC_IPR8 & 0x00FFFFFF)|0x40000000; // priority 2
-	
-	// enable Port 1 - interrupt 35 in NVIC	
-  NVIC_ISER1 = 0x00000008;
-	
+	NVIC_IPR8 = (NVIC_IPR8 & 0x00FFFFFF) | 0x40000000; // priority 2
+
+	// enable Port 1 - interrupt 35 in NVIC
+	NVIC_ISER1 = 0x00000008;
+
 	// enable interrupts  (// clear the I bit	)
-  EnableInterrupts();
-	
+	EnableInterrupts();
 }
 void Switch2_Interrupt_Init(void)
 {
 	// disable interrupts
 	DisableInterrupts();
-	
+
 	// initialize the Switch as per previous lab
 	Switch2_Init();
-  
+
 	// clear flag4 (reduce possibility of extra interrupt)
-  P1->IFG &= ~16;  // P1.4
-  
-	// arm interrupt on P1.4 
-  P1->IE |= 16;     
-	
-		// now set the pin to cause falling edge interrupt event
-	// P1.4 is falling edge event
-  P1->IES |= 16;
-	
+	P1->IFG &= ~16; // P1.4
+
+	// arm interrupt on P1.4
+	P1->IE |= 16;
+
 	// now set the pin to cause falling edge interrupt event
-  NVIC_IPR8 = (NVIC_IPR8&0x00FFFFFF)|0x40000000; // priority 2
-	
+	// P1.4 is falling edge event
+	P1->IES |= 16;
+
+	// now set the pin to cause falling edge interrupt event
+	NVIC_IPR8 = (NVIC_IPR8 & 0x00FFFFFF) | 0x40000000; // priority 2
+
 	// enable Port 1 - interrupt 35 in NVIC
-  NVIC_ISER1 = 0x00000008;
-	
+	NVIC_ISER1 = 0x00000008;
+
 	// enable interrupts  (// clear the I bit	)
-  EnableInterrupts();
-	
+	EnableInterrupts();
 }
+
 // PORT 1 IRQ Handler
 // LJBeato
 // Will be triggered if any pin on the port causes interrupt
 //
 // Derived From: Jonathan Valvano
-
-
 void PORT1_IRQHandler(void)
 {
 	float numSeconds = 0.0;
 	char temp[32];
 
 	// First we check if it came from Switch1 ?
-  if(P1->IFG & BIT1)  // we start a timer to toggle the LED1 1 second ON and 1 second OFF
+	if (P1->IFG & BIT1) // we start a timer to toggle the LED1 1 second ON and 1 second OFF
 	{
 		// acknowledge P1.1 is pressed, by setting BIT1 to zero - remember P1.1 is switch 1
 		// clear flag, acknowledge
-    ;     
+		P1->IFG &= ~BIT1;
 
+		// update state of timer
+		Timer1RunningFlag = ~Timer1RunningFlag;
+	}
 
-  }
 	// Now check to see if it came from Switch2 ?
-  if(P1->IFG & BIT4)
+	if (P1->IFG & BIT4)
 	{
 		// acknowledge P1.4 is pressed, by setting BIT4 to zero - remember P1.4 is switch 2
-    ;     // clear flag4, acknowledge
+		// clear flag4, acknowledge
+		P1->IFG &= ~BIT4;
 
-  }
+		if (Timer2RunningFlag == FALSE)
+		{
+			MillisecondCounter = 0;
+			Timer2RunningFlag = TRUE;
+			// TODO: turn on led to colors[colorIndex];
+			colorIndex = (colorIndex + 1) % 7;
+		}
+		else
+		{
+			Timer2RunningFlag = FALSE;
+			// TODO: turn off LED2
+			// TODO: print time in seconds
+		}
+	}
 }
 
 //
@@ -144,11 +154,12 @@ void PORT1_IRQHandler(void)
 //
 void Timer32_1_ISR(void)
 {
-	if (LED1_State() == FALSE )
+	if (LED1_State() == FALSE)
 	{
-		LED1_On();
+		// TODO: LED1_On();
 	}
-	else LED1_Off();
+	else
+	// TODO: LED1_Off();
 }
 
 //
@@ -158,37 +169,33 @@ void Timer32_1_ISR(void)
 //
 void Timer32_2_ISR(void)
 {
-
-		MillisecondCounter++;
-
+	MillisecondCounter++;
 }
-
 
 //
 // main
 //
 //
 //
-int main(void){
-	//initializations
+int main(void)
+{
+	// initializations
 	uart0_init();
 	uart0_put("\r\nLab5 Timer demo\r\n");
 	// Set the Timer32-2 to 2Hz (0.5 sec between interrupts)
-	//Timer32_1_Init(&Timer32_1_ISR, SystemCoreClock/2, T32DIV1); // initialize Timer A32-1;
-        ;
-        
+	Timer32_1_Init(&Timer32_1_ISR, SystemCoreClock / 2, T32DIV1); // initialize Timer A32-1
+
 	// Setup Timer32-2 with a .001 second timeout.
 	// So use DEFAULT_CLOCK_SPEED/(1/0.001) = SystemCoreClock/1000
-	//Timer32_2_Init(&Timer32_2_ISR, SystemCoreClock/1000, T32DIV1); // initialize Timer A32-1;
-	;
-    
+	Timer32_2_Init(&Timer32_2_ISR, SystemCoreClock / 1000, T32DIV1); // initialize Timer A32-1
+
 	Switch1_Interrupt_Init();
 	Switch2_Interrupt_Init();
 	LED1_Init();
 	LED2_Init();
 	EnableInterrupts();
-  while(1)
+	while (1)
 	{
-    WaitForInterrupt();
-  }
+		WaitForInterrupt();
+	}
 }
