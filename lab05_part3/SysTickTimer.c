5e4171d694838484d128fb5b5b5fadeb2ec3b27e
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-10-11 08:41:12 -0400

    Implement sysTick

--- lab05_part3/SysTickTimer.c
+++ lab05_part3/SysTickTimer.c
@@ -3,8 +3,8 @@
 #define CLOCK_SPEED 12000000
 extern uint32_t SystemCoreClock;
 
-void (*SysTickPeriodicTask)(void);   // user function
-long g_SysTickTimerCounter = 0; 
+void (*SysTickPeriodicTask)(void); // user function
+long g_SysTickTimerCounter = 0;
 
 //
 // Enable SysTick Timer
@@ -12,8 +12,8 @@ long g_SysTickTimerCounter = 0;
 void EnableSysTickTimer(void)
 {
 	// enable SysTick with core clock and interrupts
-	//SYSTICK_STCSR
-	;      
+	// SYSTICK_STCSR
+	SYSTICK_STCSR |= 0x00000007;
 }
 
 //
@@ -21,38 +21,39 @@ void EnableSysTickTimer(void)
 //
 void DisableSysTickTimer(void)
 {
-	// disable SysTick with core clock and interrupts 
+	// disable SysTick with core clock and interrupts
 	// SYSTICK_STCSR
-	;      
+	SYSTICK_STCSR &= ~0x00000007;
 }
-// numIntsPerSec is equal to the number is the 
+
+// numIntsPerSec is equal to the number is the
 // number of times per second the SysTickHandler is going to be called.
-// If you are making a clock function, you will have to double the number so 
+// If you are making a clock function, you will have to double the number so
 // you can flip the bit high and then low on successive clocks.
 // remember if you want a 100 Hz clock you will need 200 Interrupts per second
-void SysTickTimer_Init(void(*task)(void), unsigned long period)
+void SysTickTimer_Init(void (*task)(void), unsigned long period)
 {
 	long sr = 0;
 
-  sr = StartCritical();
-	
+	sr = StartCritical();
+
 	// load the interrupt service routine
-	SysTickPeriodicTask = task;						
-	
+	SysTickPeriodicTask = task;
+
 	// Control and Status Register
 	// 1) disable SysTick during setup
 	// SYSTICK_STCSR
-  ;  
-	
+	DisableSysTickTimer();
+
 	// NOTE: The STRVR - RELOAD VALUE REGISTER)is a 24 bit value
 	// 2) reload value sets period
 	// SYSTICK_STRVR
-  SYSTICK_STRVR = period-1;  
-	
+	SYSTICK_STRVR = period - 1;
+
 	// Current Value Register
 	// 3) any write to current clears it
 	// SYSTICK_STCVR
-  ;    
+	SYSTICK_STCVR = 0;
 
 	// priority 2
 	// SCB_SHPR3
@@ -60,9 +61,8 @@ void SysTickTimer_Init(void(*task)(void), unsigned long period)
 	// 23-16 PRI_14 R/W 0h Priority of system handler 14.
 	// 15-8 PRI_13 R/W 0h Priority of system handler 13.
 	// 7-0 PRI_12 R/W 0h Priority of system handler 12
-  SCB_SHPR3 = (SCB_SHPR3&0x00FFFFFF)|0x40000000; 
-	
-	
+	SCB_SHPR3 = (SCB_SHPR3 & 0x00FFFFFF) | 0x40000000;
+
 	// Control and Status Register
 	// 31-17 RESERVED
 	// 2 CLKSOURCE R 1h Clock source.
@@ -72,19 +72,16 @@ void SysTickTimer_Init(void(*task)(void), unsigned long period)
 	//	0 ENABLE R/W 0h Enable SysTick counter
 	//	0b (R/W) = Counter disabled
 
- 
-	// 4) enable SysTick with core clock and interrupts	
+	// 4) enable SysTick with core clock and interrupts
 	// SYSTICK_STCSR
-	;      // 4) enable SysTick with core clock and interrupts
+	EnableSysTickTimer();
 
 	EndCritical(sr);
 	// the client will need to Enable Interrupts
 }
 
-void SysTick_Handler (void)
+void SysTick_Handler(void)
 {
 	g_SysTickTimerCounter++; // simple counter
-	(*SysTickPeriodicTask)()	;
+	(*SysTickPeriodicTask)();
 }
-
-
