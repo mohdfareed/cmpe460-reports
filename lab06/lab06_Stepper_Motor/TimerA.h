d0ffe5d003d6abef7300b8081fd2ccbef21b8c98
trahnt <twes1jr@gmail.com>
2023-10-28 16:24:52 -0400

    removed unused files

--- lab06/lab06_Stepper_Motor/TimerA.h
+++
@@ -1,5 +0,0 @@
-int TIMER_A0_PWM_Init(uint16_t period, double percentDutyCycle, uint16_t pin);
-void TIMER_A0_PWM_DutyCycle(double percentDutyCycle, uint16_t pin);
-
-int TIMER_A2_PWM_Init(uint16_t period, double percentDutyCycle, uint16_t pin);
-void TIMER_A2_PWM_DutyCycle(double percentDutyCycle, uint16_t pin);
