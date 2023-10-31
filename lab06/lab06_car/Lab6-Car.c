d6da147fd6bcfeb7c115dec92aa6472b1039ec42
trahnt <twes1jr@gmail.com>
2023-10-31 12:22:50 -0400

    car shit

--- lab06/lab06_car/Lab6-Car.c
+++ lab06/lab06_car/Lab6-Car.c
@@ -1,9 +1,9 @@
 /*
- * Main module for testing the PWM Code for the K64F
+ * 
  *
  * Author:
  * Created:
- * Modified: Carson Clarke-Magrab <ctc7359@rit.edu>
+ * Modified:
  * LJBeato
  * 2021
  */
@@ -12,15 +12,15 @@
 #include "uart.h"
 #include "TimerA.h"
 
-#define DC1_FORWARD (BIT4)
-#define DC1_REVERSE (BIT5)
+#define DC1_FORWARD (1)
+#define DC1_REVERSE (2)
 #define DC1_ENABLE (BIT6)
 
-#define DC2_FORWARD (BIT6)
-#define DC2_REVERSE (BIT7)
+#define DC2_FORWARD (3)
+#define DC2_REVERSE (4)
 #define DC2_ENABLE (BIT7)
 
-#define SERVO (BIT1)
+#define SERVO 1
 
 /**
  * Waits for a delay (in milliseconds)
@@ -62,49 +62,62 @@ void Init_Car_Motors(void)
 
 int main(void)
 {
-	// Initialize UART and PWM
-	uart0_init();
-	Init_Car_Motors();
-	// Print welcome over serial
-	uart0_put("Running... \n\r");
-
+	// Initialize PWM
+	//Init_Car_Motors();
+    TIMER_A2_PWM_Init(60000, 0.075, SERVO);
+    //TIMER_A0_PWM_Init(300, 0.0, 1);
+    //TIMER_A0_PWM_Init(300, 0.0, 2);
+    
+    // Motor1 Enable P3.6
+//    P3->SEL0 &= ~BIT6;
+//    P3->SEL1 &= ~BIT6;
+//    P3->DIR |= BIT6;
+//    P3->OUT |= BIT6;
+    
 	for (;;)
 	{
-		// accelerate forward
-		TIMER_A2_PWM_DutyCycle(0.05, SERVO);
-		for (i = 0; i < 100; i++)
-		{
-			TIMER_A0_PWM_DutyCycle((double)i / 100.0, DC1_FORWARD);
-			TIMER_A0_PWM_DutyCycle((double)i / 100.0, DC2_FORWARD);
-			delay(10);
-		}
+        
+        delay(2000);
+        TIMER_A2_PWM_DutyCycle(0.05, SERVO);
+        delay(2000);
+        TIMER_A2_PWM_DutyCycle(0.1, SERVO);
+        
+        
+//		// accelerate forward
+//		TIMER_A2_PWM_DutyCycle(0.05, SERVO);
+//		for (i = 0; i < 100; i++)
+//		{
+//			TIMER_A0_PWM_DutyCycle((double)i / 100.0, DC1_FORWARD);
+//			TIMER_A0_PWM_DutyCycle((double)i / 100.0, DC2_FORWARD);
+//			delay(10);
+//		}
 
-		// decelerate forward
-		TIMER_A2_PWM_DutyCycle(0.075, SERVO);
-		for (i = 100; i >= 0; i--)
-		{
-			TIMER_A0_PWM_DutyCycle((double)i / 100.0, DC1_FORWARD);
-			TIMER_A0_PWM_DutyCycle((double)i / 100.0, DC2_FORWARD);
-			delay(10);
-		}
+//		// decelerate forward
+//		TIMER_A2_PWM_DutyCycle(0.075, SERVO);
+//		for (i = 100; i >= 0; i--)
+//		{
+//			TIMER_A0_PWM_DutyCycle((double)i / 100.0, DC1_FORWARD);
+//			TIMER_A0_PWM_DutyCycle((double)i / 100.0, DC2_FORWARD);
+//			delay(10);
+//		}
 
-		// accelerate in reverse
-		TIMER_A2_PWM_DutyCycle(0.1, SERVO);
-		for (i = 0; i < 100; i++)
-		{
-			TIMER_A0_PWM_DutyCycle((double)i / 100.0, DC1_REVERSE);
-			TIMER_A0_PWM_DutyCycle((double)i / 100.0, DC2_REVERSE);
-			delay(10);
-		}
+//		// accelerate in reverse
+//		TIMER_A2_PWM_DutyCycle(0.1, SERVO);
+//		for (i = 0; i < 100; i++)
+//		{
+//			TIMER_A0_PWM_DutyCycle((double)i / 100.0, DC1_REVERSE);
+//			TIMER_A0_PWM_DutyCycle((double)i / 100.0, DC2_REVERSE);
+//			delay(10);
+//		}
 
-		// decelerate in reverse
-		TIMER_A2_PWM_DutyCycle(0.075, SERVO);
-		for (i = 100; i >= 0; i--)
-		{
-			TIMER_A0_PWM_DutyCycle((double)i / 100.0, DC1_REVERSE);
-			TIMER_A0_PWM_DutyCycle((double)i / 100.0, DC2_REVERSE);
-			delay(10);
-		}
+//		// decelerate in reverse
+//		TIMER_A2_PWM_DutyCycle(0.075, SERVO);
+//		for (i = 100; i >= 0; i--)
+//		{
+//			TIMER_A0_PWM_DutyCycle((double)i / 100.0, DC1_REVERSE);
+//			TIMER_A0_PWM_DutyCycle((double)i / 100.0, DC2_REVERSE);
+//			delay(10);
+//		}
 	}
 	return 0;
 }
