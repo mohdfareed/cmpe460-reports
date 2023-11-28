d40a9419d2a9bd03515bfe1fe098a930efc6f222
trahnt <twes1jr@gmail.com>
2023-11-28 14:58:10 -0500

    fake heartbeat detected

--- lab08/code/main.c
+++ lab08/code/main.c
@@ -9,8 +9,12 @@
 #include "Timer32.h"
 
 // constants
-#define CAPTURE_FREQ (10)    // frequency of ADC data capture
-#define VOLTAGE_CUTOFF (0.0) // voltage cutoff for a heartbeat
+#define CAPTURE_FREQ (122)    // frequency of ADC data capture
+#define VOLTAGE_CUTOFF (5000) // voltage cutoff for a heartbeat
+
+unsigned int count = 10001;
+unsigned int previous = 0;
+double prev_bpm = -1;
 
 // global variables
 // uint16_t line[128];
@@ -36,18 +40,26 @@ void Timer32_1_ISR(void)
 {
     char str[30];
     unsigned int num = ADC_In();
-    double voltage = num * 2.5 / 16384.0;
-
+    
     // calculate beat per minute
-    double bpm = 0.0;
-    if (voltage > 0.0)
-    {
-        bpm = voltage;
-    }
 
-    // print bpm
-    sprintf(str, "Heart %d BPM\r\n", (int)bpm);
-    uart0_put(str);
+    if (num >= VOLTAGE_CUTOFF && previous < VOLTAGE_CUTOFF){  
+        double bpm = 7200/count;
+        int abs = bpm - prev_bpm;
+        if (abs < 0) abs = abs*-1;
+        count = 0;
+        if (bpm > 40 && bpm < 220 && abs < 11){
+            // print bpm
+            sprintf(str, "Heart %d BPM\r\n", (int)bpm);
+            uart0_put(str);
+        }
+        prev_bpm = bpm;
+    }
+    else {
+        count++;
+    }
+    previous = num;
+
 }
 
 int main(void)
@@ -58,7 +70,7 @@ int main(void)
     Timer32_1_Init(&Timer32_1_ISR, SystemCoreClock / CAPTURE_FREQ, T32DIV1);
     ADC0_InitSWTriggerCh6();
     EnableInterrupts();
-    uart0_puts("Initialized\r\n");
+    uart0_put("Initialized\r\n");
 
     while (1)
     {
