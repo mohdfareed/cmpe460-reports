ecf5ce2a36aa2b41ecd7c6248e9219d4e8977625
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-11-29 04:18:30 -0500

    Document code

--- lab08/code/main.c
+++ lab08/code/main.c
@@ -12,53 +12,56 @@
 #define CAPTURE_FREQ (122)    // frequency of ADC data capture
 #define VOLTAGE_CUTOFF (5000) // voltage cutoff for a heartbeat
 
-unsigned int count = 10001;
-unsigned int previous = 0;
-double prev_bpm = -1;
-
 // static variables
-static char str[100];
+static char str[100]; // string for UART output
+
+// global variables
+unsigned int count = 10001; // number of ADC values below voltage cutoff
+unsigned int previous = 0;  // previous ADC value
+double prev_bpm = -1;       // previous BPM
 
 // Interrupt Service Routine for Timer32-1
 void Timer32_1_ISR(void)
 {
     char str[30];
-    unsigned int num = ADC_In();
+    unsigned int num = ADC_In(); // read ADC value
 
-    // calculate beat per minute
-
+    // if previous ADC value was below voltage cutoff and current ADC
+    // value is above voltage cutoff, calculate BPM
     if (num >= VOLTAGE_CUTOFF && previous < VOLTAGE_CUTOFF)
     {
-        double bpm = 7200 / count;
+        // calculate BPM and difference from previous BPM
+        double bpm = (CAPTURE_FREQ * 60) / count;
         int abs = bpm - prev_bpm;
         if (abs < 0)
             abs = abs * -1;
-        count = 0;
+        count = 0; // reset count
+        // if BPM is between 40 and 220 and difference from previous BPM is
+        // less than 11 (stops fluctuations), output BPM
         if (bpm > 40 && bpm < 220 && abs < 11)
         {
-            // print bpm
             sprintf(str, "Heart %d BPM\r\n", (int)bpm);
             uart0_put(str);
         }
-        prev_bpm = bpm;
+        prev_bpm = bpm; // update previous BPM
     }
     else
     {
         count++;
-    }
-    previous = num;
+    }               // increment count if under voltage cutoff
+    previous = num; // update previous ADC value
 }
 
 int main(void)
 {
-    // initializations
     DisableInterrupts();
+    // initialize hardware
     uart0_init();
-    Timer32_1_Init(&Timer32_1_ISR, SystemCoreClock / CAPTURE_FREQ, T32DIV1);
     ADC0_InitSWTriggerCh6();
+    // initialize adc capture timer with capture frequency
+    Timer32_1_Init(&Timer32_1_ISR, SystemCoreClock / CAPTURE_FREQ, T32DIV1);
     EnableInterrupts();
     uart0_put("Initialized\r\n");
-
     while (1)
     {
     }
