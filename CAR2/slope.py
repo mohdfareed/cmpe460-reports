24db5c5dd7ce23be88c6088e212186b53f71f7ac
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-11-29 17:22:09 -0500

    Add code to calc slope of 2nd order func

---
+++ CAR2/slope.py
@@ -0,0 +1,14 @@
+xc = 63.5
+x1 = 61
+x2 = 66
+
+yc = 0.075
+y1 = 0.1
+y2 = 0.05
+
+# find constants for 2nd order equation ()
+# y = 1/a (x - yc) + xc
+a = ((x1 - xc) ** 2) / (y1 - yc)
+
+print(f"y = 1/a(x - {xc})^2 + {yc}")
+print(f"a = ({x1} - {xc})^2 / ({y1} - {yc}) = {a}")
