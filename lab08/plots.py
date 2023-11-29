6ef57ca2154e998f615efc30eccfffa12c66e6ed
Mohammed Fareed <mohdf.fareed@icloud.com>
2023-11-28 20:45:26 -0500

    Fix plots

--- lab08/plots.py
+++ lab08/plots.py
@@ -12,6 +12,9 @@ from numpy import pi
 # time_difference = np.array(
 #     [-1.04, -0.653, -0.28, -0.179, -0.1, -0.03, -0.004, -0.001, 0, 0]
 # )
+# cutoff_frequency = 0.7  # Hz
+# filename = "high_pass_plot.pdf"
+
 
 # low pass
 # frequency = np.array([0.2, 0.3, 0.5, 1, 2, 3.5, 5, 10, 15, 20])
@@ -20,6 +23,8 @@ from numpy import pi
 # time_difference = np.array(
 #     [0, 0, 0.04, 0.036, 0.040, 0.030, 0.026, 0.018, 0.010, 0.009]
 # )
+# cutoff_frequency = 3.5  # Hz
+# filename = "low_pass_plot.pdf"
 
 # band pass
 frequency = np.array([0.3, 0.5, 0.7, 1, 2, 3, 3.5, 5, 10, 15])
@@ -28,6 +33,9 @@ vout = np.array([6.5, 9.5, 12, 13.5, 14.5, 13, 12, 10, 6, 4])
 time_difference = np.array(
     [-3.90, -2.25, -1.55, -1.05, -0.49, -0.32, -0.266, -0.181, -0.084, -0.0547]
 )
+cutoff_frequency = 0.7
+cutoff_frequency2 = 3.5
+filename = "band_pass_plot.pdf"
 
 # Calculations
 gain = 20 * np.log10(vout / vin)  # gain in dB
@@ -35,7 +43,7 @@ phase = (time_difference * frequency) * 2 * pi  # phase in degrees
 
 # Plotting
 plt.rcParams.update({"font.size": 10, "text.usetex": True})
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(6, 8))
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
 
 # Gain plot
 ax1.semilogx(frequency, gain, "b-o")
@@ -43,6 +51,13 @@ ax1.set_title("$$\\textrm{Gain (dB) vs Frequency (Hz)}$$")
 ax1.set_xlabel("$$\\textrm{Frequency (Hz)}$$")
 ax1.set_ylabel("$$\\textrm{Gain (dB)}$$")
 ax1.grid(True, which="both", ls="--")
+ax1.axvline(
+    x=cutoff_frequency,
+    color="k",
+    linestyle="--",
+    label="$$\\textrm{Cutoff frequency}$$",
+)
+
 
 # Phase plot
 ax2.semilogx(frequency, phase, "r-o")
@@ -50,6 +65,20 @@ ax2.set_title("$$\\textrm{Phase (Radians) vs Frequency (Hz)}$$")
 ax2.set_xlabel("$$\\textrm{Frequency (Hz)}$$")
 ax2.set_ylabel("$$\\textrm{Phase (Radians)}$$")
 ax2.grid(True, which="both", ls="--")
+ax2.axvline(
+    x=cutoff_frequency,
+    color="k",
+    linestyle="--",
+    label="$$\\textrm{Cutoff frequency}$$",
+)
 
+if "cutoff_frequency2" in globals():
+    ax1.axvline(x=cutoff_frequency2, color="k", linestyle="--")
+    ax2.axvline(x=cutoff_frequency2, color="k", linestyle="--")
+
+ax1.legend()
+ax2.legend()
+
+# save plot to file
 plt.tight_layout()
-plt.show()
+plt.savefig(filename)
