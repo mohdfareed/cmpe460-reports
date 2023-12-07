234e00f26f3781d53675293244188dac014c2a2a
trahnt <twes1jr@gmail.com>
2023-12-06 22:37:35 -0500

    added car report

--- CAR2/car.c
+++ CAR2/car.c
@@ -3,108 +3,55 @@
 
 #include "msp.h"
 #include "uart.h"
+#include "Timer32.h"
 #include "CortexM.h"
 #include "Common.h"
 #include "ADC14.h"
 #include "ControlPins.h"
 #include "TimerA.h"
 
-// constants
-#define LEFT_MOTOR_FORWARD (2)
-#define LEFT_MOTOR_BACKWARD (1)
-#define RIGHT_MOTOR_FORWARD (4)
-#define RIGHT_MOTOR_BACKWARD (3)
-#define SERVO (1)
-
-// global variables
 uint16_t line[128];
 BOOLEAN g_sendData;
 
-// static variables
-static char str[100];
-static int i;
-static int sum;
-static int weighted_sum;
-static double midpoint;
-static double motorSpeed;
-static double steeringAngle;
+#define LEFT_MOTOR_FORWARD 2
+#define LEFT_MOTOR_BACKWARD 1
+#define RIGHT_MOTOR_FORWARD 4
+#define RIGHT_MOTOR_BACKWARD 3
 
-// function prototypes
-void Navigate(void);
+#define ERROR_HISTORY_LENGTH (10)
 
-/**
- * Drives the car at a given speed and direction
- *
- * speed - The speed to drive at in range [-1.0, 1.0], where 1.0 is
- *         full speed forward and -1.0 is full speed backward
- */
-void Drive(double speed)
-{
-	if (speed > 0.0)
-	{
-		TIMER_A0_PWM_DutyCycle(0.0, LEFT_MOTOR_BACKWARD);
-		TIMER_A0_PWM_DutyCycle(0.0, RIGHT_MOTOR_BACKWARD);
+#define STRAIGHT_PATH_THRESHOLD (0.8)
+#define STRAIGHT_PATH_SPEED_FACTOR (1.0)
 
-		TIMER_A0_PWM_DutyCycle(speed, LEFT_MOTOR_FORWARD);
-		TIMER_A0_PWM_DutyCycle(speed, RIGHT_MOTOR_FORWARD);
-	}
-	else
-	{
-		TIMER_A0_PWM_DutyCycle(0.0, LEFT_MOTOR_FORWARD);
-		TIMER_A0_PWM_DutyCycle(0.0, RIGHT_MOTOR_FORWARD);
+#define CURVED_PATH_THRESHOLD (2)
+#define CURVED_PATH_SPEED_FACTOR (1.0)
 
-		TIMER_A0_PWM_DutyCycle(speed, LEFT_MOTOR_BACKWARD);
-		TIMER_A0_PWM_DutyCycle(speed, RIGHT_MOTOR_BACKWARD);
-	}
-}
+#define IntegralErrorHistoryLength (3)
+int integralErrorHistoryIndex = 0;
+int integralErrorHistory[IntegralErrorHistoryLength];
+double previousError = 0;
+double previousError2 = 0;
 
-/**
- * Steers the car at a given angle
- *
- * angle - The angle to steer at in range [0.05, 0.1], where 0.05 is full
- * 	       left and 0.1 is full right
- */
-void Steer(double angle)
-{
-	TIMER_A2_PWM_DutyCycle(angle, SERVO);
-}
+double errorHistory[ERROR_HISTORY_LENGTH];
+int errorHistoryIndex = 0;
 
-/**
- * Checks if switch 1 is pressed
- *
- * Returns TRUE if pressed, FALSE otherwise
- */
-BOOLEAN Switch1Pressed(void)
-{
-	// check if pressed
-	if (P1->IN & BIT4) // if not pressed
-		return FALSE;
-	return TRUE; // if pressed
-}
+//static char str[100];
 
 /**
  * Waits for a delay (in milliseconds)
  *
- * ms - The delay in milliseconds
+ * del - The delay in milliseconds
  */
-void Delay(int ms)
+void delay_ms(int del)
 {
 	volatile int i;
-	for (i = 0; i < ms * 2224; i++)
+	for (i = 0; i < del * 2224; i++)
 	{
-	} // 2224 for 24 MHz
+		; // Do nothing
+	}
 }
 
-void InitSwitch(void)
-{
-	P1->SEL0 &= ~BIT4;
-	P1->SEL1 &= ~BIT4;
-	P1->DIR &= ~BIT4;
-	P1->REN |= BIT4;
-	P1->OUT |= BIT4;
-}
-
-void InitCamera(void)
+void INIT_Camera(void)
 {
 	g_sendData = FALSE;
 	ControlPin_SI_Init();
@@ -112,8 +59,9 @@ void InitCamera(void)
 	ADC0_InitSWTriggerCh6();
 }
 
-void InitMotors(void)
+void INIT_Motors(void)
 {
+	// Motor1 Enable P3.6
 	P3->SEL0 &= ~BIT6;
 	P3->SEL1 &= ~BIT6;
 	P3->DIR |= BIT6;
@@ -124,80 +72,157 @@ void InitMotors(void)
 	P3->DIR |= BIT7;
 	P3->OUT &= ~BIT7;
 
+	// Init_PWM_INTERRUPTS();
 	TIMER_A0_PWM_Init(2400, 0.0, 1);
 	TIMER_A0_PWM_Init(2400, 0.0, 2);
 	TIMER_A0_PWM_Init(2400, 0.0, 3);
 	TIMER_A0_PWM_Init(2400, 0.0, 4);
-	TIMER_A2_PWM_Init(60000, 0.0, 1);
+	TIMER_A2_PWM_Init(60000, 0.075, 1);
 
-	Delay(250);
+	delay_ms(250);
 	P3->OUT |= BIT6;
 	P3->OUT |= BIT7;
 }
 
+void Switch_Init(void)
+{
+	// configure PortPin for Switch 1 and Switch2 as port I/O 
+	P1->SEL0 &= ~BIT4;
+	P1->SEL1 &= ~BIT4;
+	// configure as input
+	P1->DIR &= ~BIT4;
+	P1->REN |= BIT4;
+	P1->OUT |= BIT4;   
+
+    // configure PortPin for Switch 1 and Switch2 as port I/O 
+	P1->SEL0 &= ~BIT1;
+	P1->SEL1 &= ~BIT1;
+	// configure as input
+	P1->DIR &= ~BIT1;
+	P1->REN |= BIT1;
+	P1->OUT |= BIT1;  
+}
+
+int Switch_Pressed(void)
+{ 
+    if ( P1->IN & BIT4 && P1->IN & BIT1){ // if nothing pressed
+        return 0;
+    }
+    else if (P1->IN & BIT4){    // if switch 1.1 pressed
+        return 2;
+    }
+    else {                      // if switch 1.4 pressed
+        return 1;
+    }
+}
+
+void updateErrorHistory(double error) {
+    errorHistory[errorHistoryIndex] = error;
+    errorHistoryIndex = (errorHistoryIndex + 1) % ERROR_HISTORY_LENGTH;
+}
+
 int main(void)
 {
-	// wait for start signal
-	InitSwitch();
-	while (!Switch1Pressed())
-		;
+    for (int i = 0; i <= IntegralErrorHistoryLength; i++){
+        integralErrorHistory[i] = 0;
+    }
+	int i = 0;
+    Switch_Init();
+
+    double kp = -0.0048;
+    double ki = 0.0017;
+    double kd = 0.0011;
+
+    int switchVal = 0;
+    while (1){
+        if (Switch_Pressed() == 1){
+            switchVal = 1;
+            break;
+        }
+        else if (Switch_Pressed() == 2) {
+            switchVal = 2;
+            break;
+        }
+    }
 
 	// initializations
 	DisableInterrupts();
-	uart0_init(); // for debugging
-	InitMotors();
-	InitCamera();
-	EnableInterrupts();
-	uart0_puts("Initialized\r\n");
+	uart0_init();
 
-	while (1)
-	{
-		// wait for line data
+	INIT_Motors();
+
+	INIT_Camera();
+	EnableInterrupts();
+
+	while (1){
 		if (g_sendData == TRUE)
 		{
-			// navigate and wait for next frame
-			Navigate();
+            double sum = 0.0;
+            double totalLight = 0.0;
+            double weighted_sum = 0.0;
+            double integral = 0;
+
+			for (i = 0; i < 128; i++){
+                totalLight += line[i];
+                if (line[i] > 16380){
+                    sum += 1.0;
+                    weighted_sum += i;
+                }
+			}
+            double midpoint = weighted_sum / sum;
+            double error = midpoint - 64.5;
+            integralErrorHistory[integralErrorHistoryIndex] = error;
+            integralErrorHistoryIndex = (integralErrorHistoryIndex + 1)%IntegralErrorHistoryLength;
+
+			updateErrorHistory(error);
+
+            double derivative = (error - 2*previousError + previousError2);
+            previousError2 = previousError;
+            previousError = error;
+
+            for (i = 0; i <= IntegralErrorHistoryLength; i++){
+                integral += integralErrorHistory[i];
+            }
+            integral = integral / IntegralErrorHistoryLength;
+
+            double servoVal;
+            servoVal = kp*error + 0.075;
+            servoVal += ki*integral + kd*derivative;
+            if (servoVal < 0.05) servoVal = 0.05;
+            else if (servoVal > 0.1) servoVal = 0.1;
+            TIMER_A2_PWM_DutyCycle(servoVal, 1);
+
+            if (totalLight < 1750000){
+                TIMER_A0_PWM_DutyCycle(0.0, LEFT_MOTOR_FORWARD);
+                TIMER_A0_PWM_DutyCycle(0.0, RIGHT_MOTOR_FORWARD);
+            }
+            else if (switchVal == 1) {
+                if (servoVal >= 0.075) servoVal -= 0.075;
+                else servoVal = 0.075 - servoVal;
+                double motorSpeed = -18.0*servoVal + 0.434;
+                if (motorSpeed < 0.36) motorSpeed = 0.36;
+
+				double sum = 0.0;
+				for (int i = 0; i < ERROR_HISTORY_LENGTH; i++) {
+					if (errorHistory[i] < 0) errorHistory[i]*=-1;
+					sum += errorHistory[i];
+				}
+				if ((sum / ERROR_HISTORY_LENGTH) < STRAIGHT_PATH_THRESHOLD) {
+					motorSpeed *= STRAIGHT_PATH_SPEED_FACTOR;
+				} else if ((sum/ERROR_HISTORY_LENGTH) > CURVED_PATH_THRESHOLD) {
+					motorSpeed *= CURVED_PATH_SPEED_FACTOR;
+				}
+
+                TIMER_A0_PWM_DutyCycle(motorSpeed, LEFT_MOTOR_FORWARD);
+                TIMER_A0_PWM_DutyCycle(motorSpeed, RIGHT_MOTOR_FORWARD);
+            }
+            
+            else {
+                TIMER_A0_PWM_DutyCycle(0.36, LEFT_MOTOR_FORWARD);
+                TIMER_A0_PWM_DutyCycle(0.36, RIGHT_MOTOR_FORWARD);
+            }
+
 			g_sendData = FALSE;
 		}
-		Delay(1);
-	}
-}
-
-void Navigate(void)
-{
-	// calculate line characteristics
-	sum = 0;
-	weighted_sum = 0;
-	for (i = 0; i < 128; i++)
-	{
-		sum += line[i];
-		weighted_sum += i * line[i];
-	}
-	midpoint = (double)weighted_sum / (double)sum;
-
-	// drive the car
-	if (sum < 1800000)
-	{
-		Drive(0.0);
-	}
-	else
-	{
-		motorSpeed = sum * 0.000000572 - 0.83030303;
-		Drive(motorSpeed);
-	}
-
-	// steer the car
-	if (midpoint < 60)
-	{
-		Steer(0.1);
-	}
-	else if (midpoint > 67)
-	{
-		Steer(0.05);
-	}
-	else
-	{
-		steeringAngle = -0.00714285 * midpoint + 0.528571;
-		Steer(steeringAngle);
 	}
 }
